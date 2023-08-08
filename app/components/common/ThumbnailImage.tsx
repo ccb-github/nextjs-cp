c68d29360b72e0d47b6ae4fda4ab09a2d053754c
ccb-github <photonvar@protonmail.com>
2023-08-08 21:01:11 +0800

    Optimize image

--- app/components/common/ThumbnailImage.tsx
+++ app/components/common/ThumbnailImage.tsx
@@ -1,8 +1,15 @@
-export default function ThumbnailImage() {
+import { ImageProps } from "next/image";
+import BlurImage from "./BlurImage";
+
+//TODO default img
+export default function ThumbnailImage( { src, alt, width, height}: ImageProps) {
   return (
     <a href="https://ibb.co/7xmcfgQ">
-      <img src="https://i.ibb.co/7xmcfgQ/Screenshot-from-2023-05-02-18-06-17.png" 
-           alt="Screenshot-from-2023-05-02-18-06-17" 
+      <BlurImage 
+        src={ src || "https://i.ibb.co/7xmcfgQ/Screenshot-from-2023-05-02-18-06-17.png"}
+        alt={ alt || "Screenshot-from-2023-05-02-18-06-17"} 
+        width={width}
+        height={height}
       />
     </a>
   )
